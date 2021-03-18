d84fe7d4b6d007cc0b93397c1c5817f8666757b1
yeggyseo <yeggy.seo@gmail.com>
2021-03-17 22:27:21 -0400

    prob 1 and 2 done

--- week_2/getKthNodeFromLast.py
+++ week_2/getKthNodeFromLast.py
@@ -1,2 +1,45 @@
 # gets kth node from the last node in a linked list
+# two pointer approach
 
+# O(n) time
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+
+    def append(self, value):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(value)
+
+    def get_kth_node_from_last(self, k):
+        s = self.head
+        e = self.head
+
+        for i in range(k):
+            e = e.next
+
+        while e is not None:
+            s = s.next
+            e = e.next
+
+        return s
+
+
+linked_list = LinkedList(6)
+linked_list.append(7)
+linked_list.append(8)
+# linked_list.append(4)
+# linked_list.append(5)
+# linked_list.append(8)
+# linked_list.append(1)
+
+print(linked_list.get_kth_node_from_last(2).data)
--- week_2/isAvilableOrder.py
+++ week_2/isAvilableOrder.py
@@ -1,9 +1,20 @@
+# checks if a sublist in a list.
+# converts the parent list into a hash set
+# and lookup from a sublist
+
+# O(n) time
+
+
 shop_menus = ["만두", "떡볶이", "오뎅", "사이다", "콜라"]
 shop_orders = ["오뎅", "콜라", "만두"]
 
 
 def is_available_to_order(menus, orders):
-    # 이 부분을 채워보세요!
+    s = set(menus)
+
+    for o in orders:
+        if o not in s:
+            return False
     return True
 
 
